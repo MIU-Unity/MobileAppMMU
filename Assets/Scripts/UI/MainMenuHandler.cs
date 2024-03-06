67e535a74a3fa4dabba45c1d8564a121d657babe
Son1kXDev <son1kx@vk.com>
2024-03-06 14:07:02 +0300

    Main Menu
    Created main menu scene;
    Created base MenuHandler

---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public Action OnNewGameButtonPressedAction;
+    public Action OnContinueButtonPressedAction;
+    public Action OnNewGameButtonPressedAction;
+    public Action OnNewGameButtonPressedAction;
+
+    public void OnNewGameButtonPressed() { }
+
+    public void OnContinueButtonPressed() { }
+
+    public void OnSettingsButtonPressed() { }
+
+    public void OnExitButtonPressed()
+    {
+
+    }
+
+}
