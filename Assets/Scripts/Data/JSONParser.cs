5a8e839e93e6d39429ebb87a1c4ff6682109f998
Daniil <support@danyatochka.ru>
2024-03-28 19:49:02 +0300

    feat: сделал json парсер

---
+++ Assets/Scripts/Data/JSONParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Data
+{
+    public class JSONParser<T>
+    {
+         private readonly string _path = Application.dataPath + "/Data";
+        
+        public Dictionary<string, T> Parse(string fileName)
+        {
+            string fullPath = _path + (fileName.StartsWith("/") ? fileName : "/" + fileName);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new Exception("File not exist");
+            }
+            
+            return JsonUtility.FromJson<Dictionary<string, T>>(fullPath);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/HintBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Data;
+
+namespace Gameplay
+{
+    public class HintBehaviour : MonoBehaviour
+    {
+        private DateTime _lastGetting = DateTime.Now;
+        private readonly string[] _allowedTypes = new[] { "light", "full" };
+        private readonly Dictionary<string, bool> _usedHints = new()
+        {
+            { "light", false },
+            { "full", false }
+        };
+
+        private static JSONParser<Dictionary<string, string>> _parser = new();
+        private static Dictionary<string, Dictionary<string, string>> _data = _parser.Parse("HintsData.json");
+
+        private bool CheckIsCanGet(string type)
+        {
+            bool flag = true;
+            
+            if (_usedHints[type])
+            {
+                flag = false;
+            }
+            
+            if (DateTime.Now < _lastGetting.AddSeconds(5))
+            {
+                flag = false;
+            }
+            
+            return flag;
+        }
+        
+        /// <summary>
+        /// Получение подсказки
+        /// </summary>
+        /// <param name="level">Номер уровня</param>
+        /// <param name="type">"light" | "full"</param>
+        public string Get(int level, string type)
+        {
+            if (!_allowedTypes.All(type.Contains))
+            {
+                throw new Exception($"'type' has not valid value: {type}");
+            } 
+            
+            if (!CheckIsCanGet(type))
+            {
+                throw new Exception("Подсказка недоступна");
+            }
+            
+            _usedHints[type] = true;
+            _lastGetting = DateTime.Now;
+            
+            return _data[level.ToString()][type];
+        }
+    }
+}
