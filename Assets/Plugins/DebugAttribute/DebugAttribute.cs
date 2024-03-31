fb176c3c55b822f8687624d6a498a7ef3f81805c
Son1kXDev <son1kx@vk.com>
2024-03-31 20:03:15 +0300

    feat: DebugAttribute methods documentation comment

--- Assets/Plugins/DebugAttribute/DebugAttribute.cs
+++ Assets/Plugins/DebugAttribute/DebugAttribute.cs
@@ -8,11 +8,18 @@ namespace Plugins.DebugAttribute
     {
         public readonly object[] Arguments;
 
+        /// <summary>
+        /// Attribute is used to mark methods that should be invoked when the user clicks a button in the inspector.
+        /// </summary>
         public DebugAttribute()
         {
             Arguments = new object[] {};
         }
 
+        /// <summary>
+        /// Attribute is used to mark methods that should be invoked when the user clicks a button in the inspector.
+        /// </summary>
+        /// <param name="parameters">An array of objects that will be assigned to the Arguments property.</param>
         public DebugAttribute(params object[] parameters)
         {
             Arguments = parameters;
