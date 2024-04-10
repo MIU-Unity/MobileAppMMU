2cdc163809b69942e432cdf4514e6757751ef890
Son1kXDev <son1kx@vk.com>
2024-04-10 16:53:57 +0300

    fix(Bootstrap/Singleton): replaced manual singleton with Singleton class wherever it need

--- Assets/Scripts/UI/PopupConstructor.cs
+++ Assets/Scripts/UI/PopupConstructor.cs
@@ -1,35 +1,23 @@
 using System;
+using Common.Utility;
+using JetBrains.Annotations;
+using Plugins.DebugAttribute;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI
 {
-    public class PopupConstructor : MonoBehaviour
+    public class PopupConstructor : Singleton<PopupConstructor>
     {
-        public static PopupConstructor Instance { get; private set; }
-        
         [SerializeField] private Canvas _canvas;
         [SerializeField] private Popup _popupPrefab;
 
         private Popup _currentPopup;
 
-        private void Awake()
-        {
-            if (Instance != null) Destroy(this);
-            else Instance = this;
-        }
 
-        [ContextMenu("Test Popup")]
-        private void TestPopup()
-        {
-            Open("Test Popup", "This is a test popup", () =>
-            {
-                Debug.Log("Popup Confirmed");
-            });
-        }
-        
-        public void Open(string label, string text, Action callback)
+        [Debug("Test Popup", "This is a test popup", null)]
+        public void Open(string label, string text, [CanBeNull] Action callback)
         {
             _currentPopup = Instantiate(_popupPrefab, _canvas.transform);
             _currentPopup.Initialize(label, text,
