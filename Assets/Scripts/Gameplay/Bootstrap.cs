8a80db7ab33399f6de8104876a3ce0d7162923c4
Son1kXDev <son1kx@vk.com>
2024-05-21 13:34:55 +0300

    refactor(UI): комментарий в Bootstrap.cs

--- Assets/Scripts/Gameplay/Bootstrap.cs
+++ Assets/Scripts/Gameplay/Bootstrap.cs
@@ -10,7 +10,9 @@ namespace Gameplay
             HintBehaviour.Instance.Initialize();
             AttemptsBehaviour.Instance.Initialize();
             TimerBehaviour.Instance.Initialize(1);
-            GameplayEventHandler.Instance.Initialize();
+            
+            GameplayEventHandler.Instance.Initialize(); // всегда последним
+            
             //add more instances when they release
             //add more instances when they release
         }
