bb1a0486a2f5bbc4fdca504db135fd946a2bd385
Daniil <support@danyatochka.ru>
2024-06-23 22:43:13 +0300

    prefer: удалил post метод

--- Assets/Scripts/Data/CustomHttpClient.cs
+++ Assets/Scripts/Data/CustomHttpClient.cs
@@ -28,29 +28,4 @@ public class CustomHttpClient : Singleton<CustomHttpClient>
         return request.downloadHandler.text;
         
     }
-
-    public async Task<string> Post(string url, string jsonData)
-    {
-        UnityWebRequest request = new UnityWebRequest(url, "POST");
-        byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(jsonData);
-        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        var operation = request.SendWebRequest();
-        while (!operation.isDone)
-        {
-            await Task.Yield();
-        }
-
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
-        {
-            Debug.LogError(request.error);
-            return null;
-        }
-        else
-        {
-            return request.downloadHandler.text;
-        }
-    }
 }
